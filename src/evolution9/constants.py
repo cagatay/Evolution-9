a80e7b5ba20264328b93cf73255941f144deaf52
cagatay <cagatayyuksel@gmail.com>
2010-10-11 02:11:29 +0300

    composing random midi

---
+++ src/evolution9/constants.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 10, 2010
+
+@author: cagatay.yuksel
+'''
+
+SQLITE_FILE = "evolution9.db"
+OUTPUT_DIR  = "../output/"
--- src/main.py
+++ src/main.py
@@ -1,38 +1,17 @@
-#===============================================================================
-# Created on Oct 5, 2010
-# 
-# @author: cagatay
-#===============================================================================
+'''
+Created on Oct 5, 2010
+ 
+@author: cagatay.yuksel
+'''
 
-#Import the library
-from midiutil.MidiFile3 import MIDIFile
+from random import randrange
 
-# Create the MIDIFile Object with 1 track
-MyMIDI = MIDIFile(1)
+from evolution9.midi.midiActions import createMIDI
 
-# Tracks are numbered from zero. Times are measured in beats.
-track = 0
-time = 0
+composition = list()
 
-# Add track name and tempo.
-MyMIDI.addTrackName(track, time, "Sample Track")
-MyMIDI.addTempo(track, time, 120)
+for i in range(10):
+    pitch = randrange(40, 80)
+    composition.append((1, pitch))
 
-# Add a note. addNote expects the following information:
-track = 0
-channel = 0
-pitch = 60
-time = 0
-duration = 1
-volume = 100
-
-# Now add the note.
-MyMIDI.addNote(track, channel, pitch, time, duration, volume)
-MyMIDI.addNote(track, channel, pitch, time+1, duration, volume)
-MyMIDI.addNote(track, channel, 70, time+2, duration, volume)
-MyMIDI.addNote(track, channel, 80, time+3, duration, volume)
-# And write it to disk.
-binfile = open("../output/output.mid", 'wb')
-MyMIDI.writeFile(binfile)
-binfile.close()
-
+createMIDI('output.mid', composition)
