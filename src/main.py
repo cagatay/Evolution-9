abb767606c20e517f6b66fdab0accb961c520e12
melihkarci <melihkarci@gmail.com>
2010-10-25 04:18:36 +0300

    PyBrain integration

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ from random import random
 
 from evolution9.midi.midiActions import createMIDI
 from evolution9.evolution.storage import db
+from pybrain.tools.shortcuts import buildNetwork
+
 
 if __name__ == '__main__':
     storage =  db.Database()
     storage.initDatabase()
     storage.printAll()
     composition = list()
+
 
     for i in range(10):
         pitch = randrange(40, 80)
